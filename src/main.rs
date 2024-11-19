0e9158909e2fa72ebcb9bd53e653c24878a08591
zimrustpro <zimrustpro@gmail.com>
2024-11-19 08:53:41 +0200

    bad practice: function that tries to unwrap 2 times

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let my_vec = vec![9, 0, 10];
-    let _fourth = get_fourth(&my_vec);
+    let vector = vec![None, Some(1000)];
+    try_two_unwraps(vector);
 }
 
-fn get_fourth(input: &Vec<i32>) -> i32 {
-    let fourth = input.get(3).expect("Input vector needs at least 4 items");
-    *fourth
+fn try_two_unwraps(input: Vec<Option<i32>>) {
+    println!("Index 0 is: {}", input[0].unwrap());
+    println!("Index 1 is: {}", input[1].unwrap());
 }

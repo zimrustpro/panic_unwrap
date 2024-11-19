a57431eae5ff77f696f7d65fd5790f8d8ef3843e
blessedsibanda263 <blessedsibanda263@gmail.com>
2024-11-19 08:42:02 +0200

    example: panic if there is a logic bug

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    panic!("Time to panic");
+    let my_vec = vec![8, 9, 10, 10, 55, 99];
+    print_all_three_things(my_vec);
+}
+
+fn print_all_three_things(vector: Vec<i32>) {
+    if vector.len() != 3 {
+        panic!("vector must always have three items");
+    }
+    println!("{}, {}, {}", vector[0], vector[1], vector[2]);
 }
